ef320d5b61ddfef0d4b1cac1f731e78fd97a31fa
pruizsalado <ruiz.salado.pablo@iescamas.es>
2026-03-17 15:27:23 +0000

    añado los test de tienda

--- Tienda/src/test/java/com/miempresa/entidad/TiendaTest.java
+++ Tienda/src/test/java/com/miempresa/entidad/TiendaTest.java
@@ -2,18 +2,46 @@ package com.miempresa.entidad;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TiendaTest {
+	private Tienda tienda;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		tienda = new Tienda();
 	}
+	
+    @Test
+    void testAgregarProducto() {
+    	//Inicializamos el producto que vamos a añadir
+        Producto producto = new Producto("Pan", 1.5);
+        //Agregamos el producto a la lista
+        tienda.agregarProducto(producto);
+        //Comparamos resultados
+        List<Producto> productos = tienda.obtenerProductos();
+        assertEquals(1, productos.size());
+        assertEquals("Pan", productos.get(0).getNombre());
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    void testObtenerProducto() {
+    	tienda.agregarProducto(new Producto("Pan", 0));
+    	List<Producto> productos = tienda.obtenerProductos();
+    	productos.clear();
+    	assertEquals(1, tienda.obtenerProductos().size());
+    }
+    
+    @Test
+    void testCalcularPrecioTotal() {
+    	Producto p1 = new Producto("Pan", 1.0);
+    	Producto p2 = new Producto("Huevo", 1.0);
+    	
+    	double resultadoActual = tienda.calcularPrecioTotal();
+    	double resultadoEsperado = 2.0;
+    	assertEquals(resultadoEsperado, resultadoActual);
+    }
 }
